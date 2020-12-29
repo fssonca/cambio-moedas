5148438db8cef953bf149925aef8e1de9d6e5dab
fssonca <fssonca@gmail.com>
2020-12-29 00:08:10 -0300

    trying to push responsive mode - input

--- src/components/InputValues.tsx
+++ src/components/InputValues.tsx
@@ -103,64 +103,61 @@ const InputValues: React.FC = () => {
   };
 
   return (
-    <div className="border border-primaryTXT  w-full max-w-screen-lg flex sm:flex-col">
-      <div className="h-20 border bg-yellow-500 border-primaryTXT   sm:w-1/3  w-full ">
-      <span className="text-primaryTXT">Converter</span>
-<input
-  type="text"
-  value={txtUserValue}
-  onChange={handleChange}
-  className="border border-red-500 h-10 w-full p-2 text-xl	 text-right font-medium my-1.5"
-/>
+    <div className="w-full max-w-screen-lg flex flex-col sm:flex-row">
+      <div className="h-20   sm:w-1/3  w-full ">
+        <span className="text-primaryTXT">Converter</span>
+        <input
+          type="text"
+          value={txtUserValue}
+          onChange={handleChange}
+          className="border border-gray-300 h-10 w-full p-2 text-xl	 text-right font-medium my-1.5"
+        />
       </div>
 
+      <div className="h-20   sm:w-2/3 w-full  grid grid-cols-12 ">
+        <div className="h-20   px-2 col-span-5">
+          <span className="text-primaryTXT  ">DE</span>
+          {toBRL ? Brl() : ForeignMoney(currency, dispatch)}
+        </div>
 
-<div className="h-20  bg-green-300  sm:w-2/3 w-full">
-
-<div className="h-20 	border border-primaryTXT px-2">
-        <span className="text-primaryTXT  ">DE</span>
-        {toBRL ? Brl() : ForeignMoney(currency, dispatch)}
-      </div>
-
-      <div
-        className="h-20 flex items-center justify-center	cursor-pointer"
-        onClick={() => switchOrigin()}
-      >
-        <svg
-          version="1.0"
-          xmlns="http://www.w3.org/2000/svg"
-          width="25pt"
-          height="18.875pt"
-          className="fill-current text-primaryTXT"
-          viewBox="0 0 400.000000 302.000000"
-          preserveAspectRatio="xMidYMid meet"
+        <div
+          className="h-20 flex items-center justify-center	cursor-pointer col-span-2"
+          onClick={() => switchOrigin()}
         >
-          <g
-            transform="translate(0.000000,302.000000) scale(0.100000,-0.100000)"
-            stroke="none"
+          <svg
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="25pt"
+            height="18.875pt"
+            className="fill-current text-primaryTXT"
+            viewBox="0 0 400.000000 302.000000"
+            preserveAspectRatio="xMidYMid meet"
           >
-            <path
-              d="M2740 2780 l0 -240 -1370 0 -1370 0 0 -275 0 -275 1370 0 1370 0 2
+            <g
+              transform="translate(0.000000,302.000000) scale(0.100000,-0.100000)"
+              stroke="none"
+            >
+              <path
+                d="M2740 2780 l0 -240 -1370 0 -1370 0 0 -275 0 -275 1370 0 1370 0 2
 -243 3 -244 627 376 c345 207 627 381 627 386 0 9 -1236 755 -1252 755 -4 0
 -7 -108 -7 -240z"
-            />
-            <path
-              d="M628 1140 c-345 -206 -626 -379 -624 -385 2 -10 1234 -755 1248 -755
+              />
+              <path
+                d="M628 1140 c-345 -206 -626 -379 -624 -385 2 -10 1234 -755 1248 -755
 4 0 8 108 8 240 l0 240 1370 0 1370 0 0 275 0 275 -1370 0 -1370 0 -2 243 -3
 243 -627 -376z"
-            />
-          </g>  
-        </svg>
+              />
+            </g>
+          </svg>
+        </div>
+
+        <div className="h-20   px-2 col-span-5">
+          <span className="text-primaryTXT ">PARA</span>
+
+          {toBRL ? ForeignMoney(currency, dispatch) : Brl()}
+        </div>
       </div>
-
-      <div className="h-20 	border border-primaryTXT px-2">
-        <span className="text-primaryTXT ">PARA</span>
-
-        {toBRL ? ForeignMoney(currency, dispatch) : Brl()}
-      </div>
-</div>
-
-</div>
+    </div>
   );
 };
 
