0ad072caba0c70c8029e7b02566cc5d2a90e831f
Fernando Sousa <fsonca@gmail.com>
2024-02-01 10:27:47 -0300

    fix: delete redundant page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/index.css";
 import { StateProvider } from "../store";
 
 const Layout = ({ children }) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import "../css/index.css";
+import InputValues from "../components/InputValues";
+import Modal from "../components/ModalCurrencies";
+import SwitchThemeButton from "../components/SwitchThemeButton";
+import ResultConvert from "../components/resultConvert";
+import Footer from "../components/Footer";
+import Boxes from "../components/Boxes";
+import useCurrencyData from "../hooks/useCurrencyData";
 
-import { StateProvider } from "../store";
-import MyPage from "./MyPage"
+const MyPage: React.FC = () => {
+  const { theme, rates, currency } = useCurrencyData();
 
-function App() {
+  if (!rates) return <div className="text-center m-5">Loading...</div>;
+
+  const rate = rates && currency ? Math.min(rates[currency].ask, rates[currency].bid) : null;
+
   return (
-    <StateProvider>
-      <MyPage />
-    </StateProvider>
+    <div className={theme === "LIGHT" ? "theme-light" : "theme-dark"}>
+      <div className="bg-primaryBG transition-all h-full min-h-screen w-screen flex flex-col items-center p-2">
+        <SwitchThemeButton />
+        <Modal />
+        <InputValues />
+
+        {rate && (
+          <div className="text-primaryTXT text-xs my-3 sm:my-1 flex flex-col">
+            <span>
+              1 {currency} = {rate} BRL
+            </span>
+            {currency !== "BTC" && rate !== null && (
+              <span>
+                1 BRL = {(1 / rate).toFixed(4)} {currency}
+              </span>
+            )}
+          </div>
+        )}
+
+        <ResultConvert />
+        <Boxes />
+        <Footer />
+      </div>
+    </div>
   );
-}
-export default App;
+};
+
+export default MyPage;
