c0b32e07838b5293ebb259b1acdba8d8c88423d8
Fernando Sousa <fsonca@gmail.com>
2024-02-01 14:38:51 -0300

    fix: lint

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,32 +1,31 @@
 export interface Rates {
-    [key: string]: {
-      bid: number;
-      ask: number;
-      timestamp: number;
-    };
-  }
-  
-  export interface State {
-    theme: "DARK" | "LIGHT";
-    modalOpen: boolean;
-    currency: string;
-    toBRL: boolean;
-    rates: Rates | null;
-    value: number;
-  }
-  
-  export type Action = {
-    type: string;
-    payload?: any;
+  [key: string]: {
+    bid: number;
+    ask: number;
+    timestamp: number;
   };
-  
-  export interface ContextProps {
-    state: State;
-    dispatch: ({ type, payload }: { type: string; payload?: any }) => void;
-  }
-  
-  export type Money = {
-    code: string;
-    name: string;
-  };
-  
+}
+
+export interface State {
+  theme: "DARK" | "LIGHT";
+  modalOpen: boolean;
+  currency: string;
+  toBRL: boolean;
+  rates: Rates | null;
+  value: number;
+}
+
+export type Action = {
+  type: string;
+  payload?: any;
+};
+
+export interface ContextProps {
+  state: State;
+  dispatch: ({ type, payload }: { type: string; payload?: any }) => void;
+}
+
+export type Money = {
+  code: string;
+  name: string;
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,26 +1,27 @@
 import { Money } from "../types";
 
-export const CURRENCIES_API = "https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL,BTC-BRL,CAD-BRL,GBP-BRL";
+export const CURRENCIES_API =
+  "https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL,BTC-BRL,CAD-BRL,GBP-BRL";
 
 export const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-  export const currencies: Money[] = [
-    { code: "USD", name: "American Dolar" },
-    { code: "EUR", name: "Euro" },
-    { code: "GBP", name: "Pound Sterling" },
-    { code: "CAD", name: "Canadian Dolar" },
-    { code: "BTC", name: "Bitcoin" },
-  ];
+export const currencies: Money[] = [
+  { code: "USD", name: "American Dolar" },
+  { code: "EUR", name: "Euro" },
+  { code: "GBP", name: "Pound Sterling" },
+  { code: "CAD", name: "Canadian Dolar" },
+  { code: "BTC", name: "Bitcoin" },
+];
